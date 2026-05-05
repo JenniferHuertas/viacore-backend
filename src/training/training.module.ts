9bd9dfa3c822ffc3b4ab24ef5abda0b943b650d3
Florencia Cracogna <cra.florenciacracogna@gmail.com>
2026-05-05 15:14:09 -0300

    feat: configurar training module

--- src/training/training.module.ts
+++ src/training/training.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { TrainingService } from './training.service';
 import { TrainingController } from './training.controller';
+import { TrainingRepository } from './training.repository';
+import { Training } from './entities/training.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Training])],
   controllers: [TrainingController],
-  providers: [TrainingService],
+  providers: [TrainingService, TrainingRepository],
 })
 export class TrainingModule {}
