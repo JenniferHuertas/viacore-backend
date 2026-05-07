aed0eee093c1bcf5c9273a1a2d9e2e0627fef805
TFColmenares <colmenares8093@gmail.com>
2026-05-06 19:49:30 -0500

    Arreglo de conexion base de datos y agrego 2 entidades nuevas

--- src/config/typeorm.ts
+++ src/config/typeorm.ts
@@ -7,13 +7,21 @@ dotenvConfig({ path: '.development.env' });
 
 export const typeormConfig: DataSourceOptions = {
   type: 'postgres',
+
   host: process.env.DB_HOST,
   port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 5432,
+
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+
+  ssl: {
+    rejectUnauthorized: false,
+  },
+
   dropSchema: true,
   synchronize: true,
+
   entities: ['dist/**/*.entity{.ts,.js}'],
   migrations: ['dist/migrations/*{.ts,.js}'],
 };
