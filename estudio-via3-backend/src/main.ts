bc51c8bd187d1e91cf1f67d40b25c436dc6fff39
Daniel Medina <danielmedinamauricio92@gmail.com>
2026-05-14 16:49:05 -0300

    feat notifications emails and training workflow

---
+++ estudio-via3-backend/src/main.ts
@@ -0,0 +1,47 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  const swaggerDoc = new DocumentBuilder()
+    .setTitle('Proyect-Backend')
+    .setDescription('This is an API for an E-commerce')
+    .setVersion('1.0.0')
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+      },
+      'Bearer',
+    )
+    .build();
+
+  const documentModule = SwaggerModule.createDocument(app, swaggerDoc, {
+    extraModels: [],
+  });
+
+  documentModule.tags = [{ name: 'Auth' }, { name: 'Users' }];
+
+  SwaggerModule.setup('Docs', app, documentModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+    }),
+  );
+
+  await app.listen(process.env.PORT ?? 8000);
+}
+bootstrap();
