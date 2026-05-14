5f672ec229d3be3791dd06afa67cead57c60f0cf
AgustinHeizenreder <agusheizen@gmail.com>
2026-05-14 13:54:18 -0300

    module import

--- src/meetings/meetings.module.ts
+++ src/meetings/meetings.module.ts
@@ -3,10 +3,11 @@ import { MeetingsService } from './meetings.service';
 import { MeetingsController } from './meetings.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Meetings } from './entities/meeting.entity';
+import { CalendlyService } from 'src/calendly/calendly.service';
 
 @Module({
   controllers: [MeetingsController],
   providers: [MeetingsService],
-  imports: [TypeOrmModule.forFeature([Meetings])]
+  imports: [TypeOrmModule.forFeature([Meetings]), CalendlyService]
 })
 export class MeetingsModule {}
