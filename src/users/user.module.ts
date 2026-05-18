4bb37d455cacd17966e49decb346d1aa4a538494
TFColmenares <131204344+TFColmenares@users.noreply.github.com>
2026-05-18 09:45:55 -0500

    Add JwtModule to UsersModule imports

--- src/users/user.module.ts
+++ src/users/user.module.ts
@@ -3,9 +3,11 @@ import { UsersService } from './user.service';
 import { UsersController } from './user.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Users } from './entities/user.entity';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Users])],
+  JwtModule,
   controllers: [UsersController],
   providers: [UsersService],
 })
